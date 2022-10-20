3b8f0a0f077b030722effced013e170be2e3b058
Erick Sorto <SortoErick33@gmail.com>
2022-10-19 23:27:31 -0400

    Will begin transition from volley library to retrofit

--- app/src/main/java/org/ballistic/dreamjournalai/feature_dream/domain/use_case/AIResponse.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/feature_dream/domain/use_case/AIResponse.kt
@@ -1,10 +1,12 @@
 package org.ballistic.dreamjournalai.feature_dream.domain.use_case
 
+import android.content.Context
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -23,7 +25,8 @@ class AIResponse(userDream: String) {
                 "\n\n###\n\n" // add more examples with this as pad token
 
     fun requestResponse(): String {
-
+        //volleyRequestQueue = Volley.newRequestQueue()
+        //volleyRequestQueue.start()
         val engine = "text-davinci-002"
         val apiKey = org.ballistic.dreamjournalai.BuildConfig.API_KEY
         //sends request to OpenAI
