7cc594620dec9bd281bd18ead49cc0f217a75b84
naar0x <mohamedamine.ib@gmail.com>
2022-11-01 12:40:35 +0100

    added a comment in OpenAIRepositoryImpl.kt with explanation on how to make the method better

--- app/src/main/java/org/ballistic/dreamjournalai/feature_dream/data/repository/OpenAIRepositoryImpl.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/feature_dream/data/repository/OpenAIRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.ballistic.dreamjournalai.feature_dream.data.repository
 
+import org.ballistic.dreamjournalai.core.Resource
 import org.ballistic.dreamjournalai.feature_dream.data.remote.OpenAIApi
 import org.ballistic.dreamjournalai.feature_dream.data.remote.dto.CompletionDTO
 import org.ballistic.dreamjournalai.feature_dream.domain.model.Prompt
@@ -13,6 +14,25 @@ class OpenAIRepositoryImpl @Inject constructor(
     override suspend fun getCompletion(
         prompt: Prompt
     ): CompletionDTO {
+        //AMINE
+        //check the api response first, and see if result.isSuccessful is true,
+        //handle the use case of failure too, this is where the  Resource class that you have in core comes in handy
+        //i'll leave an example of usage here
+        //You'll have to change the return type of the function to make it Result<Type>
+        //in functions that use this (typically in your use case), u'll return the same object but either with your data
+        //in case the fetch was successfull, or in the opposite case, you'll return a Result.Error(message="There was an error while fetching the data")
+        //you can also be more specific and see what is the error code returned in api.getCompletion(prompt).code()
+        //this way you can be more direct about the error message, for example 5xx is server side error, 4xx network error etc
+        /*
+            var data : Resource<Any>? = null
+            var result = api.getCompletion(prompt)
+            if(result.isSuccessful){
+                data = Resource.Success(data = result.body()!!)
+            }else{
+                data = Resource.Error(message = result.message(), data = result.errorBody())
+            }
+            return data
+        */
         return api.getCompletion(prompt).body()!!
     }
 }
