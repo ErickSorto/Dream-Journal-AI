5032cc519a8f986cc70db0e56984b2185ca88a1b
naar0x <mohamedamine.ib@gmail.com>
2022-11-03 21:22:46 +0100

    Refactor data-repo

--- app/src/main/java/org/ballistic/dreamjournalai/di/OpenAIModule.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/di/OpenAIModule.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.ballistic.dreamjournalai.feature_dream.data.remote.OpenAIApi
 import org.ballistic.dreamjournalai.feature_dream.data.repository.OpenAIRepositoryImpl
 import org.ballistic.dreamjournalai.feature_dream.domain.repository.OpenAIRepository
@@ -14,13 +16,29 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object OpenAIModule {
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+
+
     @Provides
     @Singleton
     fun provideOpenAIApi(
+        okHttpClient: OkHttpClient
     ): OpenAIApi {
         return Retrofit.Builder()
             .baseUrl("https://api.openai.com/v1/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(OpenAIApi::class.java)
     }
