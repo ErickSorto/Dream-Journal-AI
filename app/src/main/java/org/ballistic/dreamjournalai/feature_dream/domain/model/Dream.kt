9bfa6a551d87e0a3bf016a99a981ec9c2f1f59a7
erick <Ninjaballista3@gmail.com>
2022-12-15 07:54:22 -0500

    updated background images

--- app/src/main/java/org/ballistic/dreamjournalai/feature_dream/domain/model/Dream.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/feature_dream/domain/model/Dream.kt
@@ -24,7 +24,7 @@ data class Dream(
     val generatedDetails: String,
     @PrimaryKey val id: Int? = null
 ) {
-    companion object { //backgroundss
+    companion object { //backgroundsss
         val dreamBackgroundImages = listOf(
 
             R.drawable.pink_river,
