c66cf7496a6c0e47f33aa74b7f6d3651f5aff319
Erick Sorto <sortoerick33@gmail.com>
2023-01-02 21:48:15 -0500

    Added sign in and authentication

---
+++ app/src/main/java/org/ballistic/dreamjournalai/feature_dream/presentation/signup_screen/SignInScreen.kt
@@ -0,0 +1,84 @@
+package org.ballistic.dreamjournalai.feature_dream.presentation.signup_screen
+
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.google.android.gms.auth.api.identity.BeginSignInResult
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
+import org.ballistic.dreamjournalai.feature_dream.navigation.Screens
+import org.ballistic.dreamjournalai.feature_dream.presentation.signup_screen.components.OneTapSignIn
+import org.ballistic.dreamjournalai.feature_dream.presentation.signup_screen.components.SignInButton
+import org.ballistic.dreamjournalai.feature_dream.presentation.signup_screen.components.SignInWithGoogle
+
+@Composable
+fun Sign_In_Screen(
+    viewModel: AuthViewModel = hiltViewModel(),
+    navController: NavHostController
+) {
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SignInButton(
+                onClick = {
+                    viewModel.oneTapSignIn()
+                }
+            )
+        }
+
+    }
+
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                try {
+                    val credentials =
+                        viewModel.oneTapClient.getSignInCredentialFromIntent(result.data)
+                    val googleIdToken = credentials.googleIdToken
+                    val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+                    viewModel.signInWithGoogle(googleCredentials)
+                } catch (it: ApiException) {
+                    print(it)
+                }
+            }
+        }
+
+    fun launch(signInResult: BeginSignInResult) {
+        val intent = IntentSenderRequest.Builder(signInResult.pendingIntent.intentSender).build()
+        launcher.launch(intent)
+    }
+
+
+    OneTapSignIn(
+        launch = {
+            launch(it)
+        }
+    )
+
+    SignInWithGoogle(
+        navigateToHomeScreen = { signedIn ->
+            if (signedIn) {
+                navController.navigate(Screens.StoreScreen.route)
+            }
+        }
+    )
+}
+
+
