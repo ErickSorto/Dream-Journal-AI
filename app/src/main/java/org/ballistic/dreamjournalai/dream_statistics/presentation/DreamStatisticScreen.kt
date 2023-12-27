397fa05049caa75b6b39ea0a7ef3a99bd1ed9f23
Erick Sorto <SortoErick33@gmail.com>
2023-12-27 16:31:15 -0500

    Add BarChart in DreamStatisticScreen.kt

--- app/src/main/java/org/ballistic/dreamjournalai/dream_statistics/presentation/DreamStatisticScreen.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/dream_statistics/presentation/DreamStatisticScreen.kt
@@ -1,15 +1,22 @@
 package org.ballistic.dreamjournalai.dream_statistics.presentation
 
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.column.columnChart
+import com.patrykandpatrick.vico.core.axis.Axis
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.component.marker.MarkerComponent
+import com.patrykandpatrick.vico.core.component.text.TextComponent
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import com.patrykandpatrick.vico.core.entry.entryOf
 import org.ballistic.dreamjournalai.dream_statistics.StatisticEvent
@@ -20,14 +27,17 @@ fun DreamStatisticScreen(
     dreamStatisticScreenState: DreamStatisticScreenState,
     onEvent: (StatisticEvent) -> Unit
 ) {
-    LaunchedEffect(key1 = Unit){
+    LaunchedEffect(key1 = Unit) {
         onEvent(StatisticEvent.LoadDreams)
     }
+    val dreamTypeLabels =
+        listOf("Lucid", "Normal", "Nightmare", "Favorite", "Recurring", "False Awakening")
+    val barColors =
+        listOf(Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Magenta, Color.Cyan)
 
-    //public fun entryOf(x: Float, y: Float): FloatEntry = FloatEntry(x, y)
-    //the y should be the highest number of the map
-    val chartEntryModelProducer = ChartEntryModelProducer(
-        listOf(
+
+    if (dreamStatisticScreenState.dreams.isNotEmpty()) {
+        val entries = listOf(
             entryOf(0, dreamStatisticScreenState.totalLucidDreams),
             entryOf(1, dreamStatisticScreenState.totalNormalDreams),
             entryOf(2, dreamStatisticScreenState.totalNightmares),
@@ -35,21 +45,33 @@ fun DreamStatisticScreen(
             entryOf(4, dreamStatisticScreenState.totalRecurringDreams),
             entryOf(5, dreamStatisticScreenState.totalFalseAwakenings),
         )
-    )
+        val chartEntryModelProducer = ChartEntryModelProducer(
+            entries
+        )
+        val horizontalAxisValueFormatter =
+            AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, _ ->
+                dreamTypeLabels.getOrNull(value.toInt()) ?: ""
+            }
+        val bottomAxis = rememberBottomAxis().apply {
+            valueFormatter = horizontalAxisValueFormatter
+            labelRotationDegrees = 90f
+            sizeConstraint = Axis.SizeConstraint.TextWidth("False Awakening")
+        }
 
-    if (dreamStatisticScreenState.dreams.isEmpty()) {
-        return
-    } else {
         Chart(
             chart = columnChart(),
             chartModelProducer = chartEntryModelProducer,
             startAxis = rememberStartAxis(),
-            bottomAxis = rememberBottomAxis(),
-            modifier = Modifier.padding(16.dp),
+            bottomAxis = bottomAxis,
+            marker = MarkerComponent(
+                guideline = null,
+                indicator = null,
+                label = TextComponent.Builder().build(),
+            ),
+            modifier = Modifier
+                .padding(32.dp)
+                .height(300.dp),
             isZoomEnabled = false,
         )
     }
-
-
-
 }
