6af29bf16333d4e2604093f887c5e6739c000c01
Erick Sorto <SortoErick33@gmail.com>
2024-01-02 18:27:50 -0500

    Customize Top Word PieChart

--- app/src/main/java/org/ballistic/dreamjournalai/dream_statistics/StatisticEvent.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/dream_statistics/StatisticEvent.kt
@@ -1,6 +1,9 @@
 package org.ballistic.dreamjournalai.dream_statistics
 
+import android.content.Context
+
 sealed class StatisticEvent {
     data object LoadDreams : StatisticEvent()
     data object LoadStatistics : StatisticEvent()
+    data object LoadDictionary : StatisticEvent()
 }
--- app/src/main/java/org/ballistic/dreamjournalai/dream_statistics/presentation/DreamStatisticScreen.kt
+++ app/src/main/java/org/ballistic/dreamjournalai/dream_statistics/presentation/DreamStatisticScreen.kt
@@ -3,15 +3,21 @@ package org.ballistic.dreamjournalai.dream_statistics.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import org.ballistic.dreamjournalai.dream_statistics.StatisticEvent
 import org.ballistic.dreamjournalai.dream_statistics.presentation.components.DreamChartBarChart
 import org.ballistic.dreamjournalai.dream_statistics.presentation.components.DreamStatisticScreenTopBar
+import org.ballistic.dreamjournalai.dream_statistics.presentation.components.TopFiveDreamWordPieChart
 import org.ballistic.dreamjournalai.dream_statistics.presentation.viewmodel.DreamStatisticScreenState
 import org.ballistic.dreamjournalai.feature_dream.presentation.main_screen.viewmodel.MainScreenViewModelState
 
@@ -21,6 +27,7 @@ fun DreamStatisticScreen(
     mainScreenViewModelState: MainScreenViewModelState,
     onEvent: (StatisticEvent) -> Unit
 ) {
+    val context = LocalContext.current
     LaunchedEffect(key1 = Unit) {
         onEvent(StatisticEvent.LoadDreams)
     }
@@ -35,8 +42,11 @@ fun DreamStatisticScreen(
             Modifier
                 .fillMaxSize()
                 .padding(it)
+                .navigationBarsPadding()
+                .verticalScroll(rememberScrollState())
         ) {
-            DreamChartBarChart(dreamStatisticScreenState)
+            DreamChartBarChart(dreamStatisticScreenState = dreamStatisticScreenState)
+            TopFiveDreamWordPieChart(dreamStatisticScreenState = dreamStatisticScreenState)
         }
     }
 }
