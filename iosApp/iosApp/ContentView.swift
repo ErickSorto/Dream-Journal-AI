797fcbbd5b67c3cb8d21707bd86bff8b6b62b666
ericksorto <sortoerick33@gmail.com>
2025-01-15 11:07:42 -0500

    Migrate to KMM

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Erick Sorto on 1/15/25.
+//
+
+import UIKit
+import SwiftUI
+import ComposeApp
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+                .ignoresSafeArea(edges: .all)
+                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+    }
+}
